50406f29287767a8c3aa475e0bd6bc264acd4ef2
Rob Ranf <robranf@gmail.com>
2020-08-12 15:29:10 -0700

    dep adjustments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Main msg="This is the Madison Dashboard" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
+import Main from "@/components/Main.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
+    Main,
     HelloWorld,
   },
 };
