590d56d00847728f22d8f9891a4b4eb730ff4d69
Rob Ranf <robranf@gmail.com>
2020-08-24 16:58:55 -0700

    adding empty components

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p class="font-bold">This is a bold sentence.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <Main msg="This is the Madison Dashboard" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Main from "@/components/Main.vue";
 
 export default {
   name: "Home",
   components: {
-    Main,
-    HelloWorld,
-  },
+    Main
+  }
 };
 </script>
